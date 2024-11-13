b024f97cb31bf634213abb213da77b5ae6b949c7
Connerr14 <conner7recoskie@gmail.com>
2024-11-13 09:11:44 -0500

    Added employee class

--- src/main/java/com/example/jsonproject/HelloApplication.java
+++ src/main/java/com/example/jsonproject/HelloApplication.java
@@ -1,9 +1,11 @@
 package com.example.jsonproject;
 
+import com.google.gson.stream.JsonReader;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import com.google.gson.Gson;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -28,9 +30,15 @@ public class HelloApplication extends Application {
         (
                 // Create the file reader object
                 FileReader fileReader = new FileReader("src/main/resources/com/example/jsonproject/employees.json");
-
+                // Create the jsonReader object
+                JsonReader jsonReader = new JsonReader(fileReader);
         )
         {
+            // Make a gson object from the Gson class
+            Gson gson = new Gson();
+
+            // Read employees from json response
+            EmployeesResponse employeesResponse = gson.fromJson(jsonReader, EmployeesResponse.class);
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.jsonproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
 
     opens com.example.jsonproject to javafx.fxml;
